602e1cbab33651a7a8a1dab43314f8756c921026
cjdepo <cjdepowell@protonmail.com>
2021-12-04 22:08:04 -0500

    almost finished

--- linked_list.rb
+++ linked_list.rb
@@ -13,7 +13,6 @@ class LinkedList
             current_node = current_node.next_node
         end
         new_node = Node.new(value)
-        new_node.value = value
         new_node.next_node = nil
         if prev_node != nil
             prev_node.next_node = new_node
@@ -25,7 +24,109 @@ class LinkedList
 
 
     def prepend(value)
+        
+        new_node = Node.new(value)
+        new_node.next_node = @head
+        @head = new_node
+
     end
+
+    def size
+        x = 0
+        current_node = @head
+        while current_node != nil
+            x += 1
+            current_node = current_node.next_node
+        end
+        x
+    end
+
+    def tail
+        current_node = @head
+        while current_node != nil
+            prev_node = current_node
+            current_node = current_node.next_node
+        end
+        prev_node
+    end
+
+    def at(index)
+        x = 1
+        current_node = @head
+        while x != index
+            x += 1
+            if current_node == nil
+                return "No node at this index!"
+            end
+            current_node = current_node.next_node
+        end
+        current_node
+    end
+            
+    def pop
+        if @head == nil
+            return "Empty List"
+        elsif @head.next_node == nil
+            @head = nil
+        else 
+            current_node = @head
+            until current_node.next_node == nil
+                prev_node = current_node
+                current_node = current_node.next_node
+            end
+            prev_node.next_node = nil
+        end
+    end     
+    
+    def contains?(value)
+        current_node = @head
+        while current_node != nil
+            if current_node.value == value
+                return true
+            end
+            current_node = current_node.next_node
+        end
+        false
+    end
+
+    def find(value)
+        current_node = @head
+        x = 1
+        v = nil
+        while current_node != nil
+            if current_node.value == value
+                v = x
+                return v
+            else
+                current_node = current_node.next_node
+                x += 1
+            end
+        end
+        if v == nil
+            nil
+        end
+    end
+
+    def to_s
+        str = ''
+        current_node = @head
+        while current_node != nil
+            str << "( #{current_node.value} ) ->"
+            current_node = current_node.next_node
+        end
+        str << ' nil'    
+        str
+    end
+
+    def insert_at(value, index)
+        x = 1
+        current_node = @head
+        while current_node != index
+            current_node = current_node.next_node
+    end
+
+
+
 
 end
 
@@ -41,7 +142,14 @@ class Node
 end
 
 linky = LinkedList.new
-linky.append(8)
-linky.append(8)
-p linky.head
-p linky
+linky.append(2)
+linky.append(3)
+linky.prepend(1)
+p linky.contains?(3)
+p linky.find(2)
+p linky.to_s
+p linky.tail
+p linky.at(3)
+linky.pop; p linky
+linky.pop; p linky
+linky.pop; p linky
